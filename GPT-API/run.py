08e5b51d1b336453c88101f225f7e7d01cbfaf62
S@LAT <shrainerilya@mail.ru>
2025-03-11 11:23:06 +0500

    мелкие изменения

--- GPT-API/run.py
+++ GPT-API/run.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from g4f.client import Client
-import g4f
+import os
 import asyncio
 from openai import OpenAI
 token = "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"
@@ -20,32 +20,6 @@ app.add_middleware(
     allow_methods=["GET", "OPTIONS"],  # Разрешенные методы
     allow_headers=["*"],  # Разрешенные заголовки
 )
-
-
-
-@app.get("/GPT/{prompt}", tags=['Запрос чату гпт'], summary='chatGPT')
-def g4f_generate_answer_gpt(prompt: str):
-    client = Client()
-    response = g4f.ChatCompletion.create(
-        model="gpt-4",
-        messages=[{"role": "user", "content": prompt}],
-        stream=False  # Ожидаем весь ответ сразу
-    )
-    response = response.replace("[Login to OpenAI ChatGPT]", "")
-    # Проверяем, является ли response генератором
-    if isinstance(response, str):
-        print('Вернулся правильный ответ')
-        return response.replace('\n', '<br>')  # Если это строка, просто заменяем переносы строк
-
-    elif isinstance(response, dict) and "content" in response:
-        print('Вернулся словарь')
-        return response["content"].replace('\n', '<br>')  # Если это словарь, берем content
-    elif isinstance(response, list):  # Если это список частей ответа
-        print('Пришлось собирать ответ')
-        full_response = "".join([chunk.get("content", "") for chunk in response])
-        return full_response.replace('\n', '<br>')
-
-    return "⚠ Ошибка: пустой ответ от GPT."
 
 @app.get("/DeepSeek/{prompt}", tags=['Запрос чату гпт_DeepSeek'], summary='DeepSeek')
 def DeepSeek_generate_answer_gpt(prompt: str):
@@ -69,4 +43,5 @@ def DeepSeek_generate_answer_gpt(prompt: str):
         )
     return(completion.choices[0].message.content)
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    port = int(os.getenv("PORT", 8000))  # Railway предоставляет порт через переменную окружения
+    uvicorn.run(app, host="0.0.0.0", port=port)

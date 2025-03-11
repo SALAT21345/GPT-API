05972a5df16ae925d0d81aefc18f2956667819e7
S@LAT <shrainerilya@mail.ru>
2025-03-11 15:31:56 +0500

    s

--- run.py
+++ run.py
@@ -3,10 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 # import uvicorn
 from g4f.client import Client
 import os
-os.environ["OPENROUTER_API_KEY"] = "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"
-from openai import OpenAI
-token = os.getenv("OPENROUTER_API_KEY")
 
+from openai import OpenAI
+
+token = os.getenv("OPENROUTER_API_KEY")
+if not token:
+    raise ValueError("API-ключ не найден в переменных окружения!")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=token,
+)
 app = FastAPI()
 
 # Настройка CORS
@@ -21,10 +28,6 @@ app.add_middleware(
 
 @app.get("/DeepSeek/{prompt}", tags=['Запрос чату гпт_DeepSeek'], summary='DeepSeek')
 def DeepSeek_generate_answer_gpt(prompt: str):
-    client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=token,
-    )
     completion = client.chat.completions.create(
     model="deepseek/deepseek-r1:free",
     messages=[{"role": "user", "content": prompt}]
